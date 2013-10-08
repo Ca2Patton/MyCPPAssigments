070ed353aad1b5ea291d059b0d09bab54373966e
Caitlin Ann Patton <kittyluva2@gmail.com>
2013-10-08 13:59:56 -0700

    Working PhoneNumbers.cc

--- umlTest/PhoneNumbers.cc
+++ umlTest/PhoneNumbers.cc
@@ -4,8 +4,10 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 
-class PhoneNumber { //This class will help the user make a list of phone numbers)
+class PhoneNumber 
+{ //This class will help the user make a list of phone numbers)
 	private:
 		int countryCode; //Stores the Country Code e.g. +1
 		int areaCode; //Stores Area Code e.g. (925)
@@ -34,9 +36,15 @@ class PhoneNumber { //This class will help the user make a list of phone numbers
 
 //The Default Constructor
 PhoneNumber::PhoneNumber() {
+	setCountry(43);
+	setArea(800);
+	setNumber(8675309); 
+	setType('H');
+	setYear(1981);
 }
 
 //Fill out the Phone Number class
+/* 
 PhoneNumber::PhoneNumber(int ccode, int acode, int num, char line, int year) {
 }
 
@@ -45,21 +53,25 @@ PhoneNumber::PhoneNumber(int num, char line) {
 
 PhoneNumber::PhoneNumber (int acode, int num, char line) {
 }
-
+*/
 void PhoneNumber::setCountry(int ccode) {
-
+	countryCode = ccode;
 }
 
 void PhoneNumber::setArea(int acode) {
+	areaCode = acode;
 }
 
 void PhoneNumber::setNumber(int num) {
+	number = num;	
 }
 
 void PhoneNumber::setType(char line) {
+	type = line;
 }
 
-void PhoneNumber::setYear(int year) {
+void PhoneNumber::setYear(int yyyy) {
+	year = yyyy;
 }
 
 int PhoneNumber::getCountry() const {
@@ -83,26 +95,27 @@ int PhoneNumber::getYear() const {
 }
 
 bool PhoneNumber::doubleDigits() const {
-
+return true;
 }
 
 void PhoneNumber::printNumber() const {
-
+	cout << number << endl;
 }
 
 void PhoneNumber::printPhoneNumberStats() const {
-
+return;
 }
 
 int main() {
+
 	PhoneNumber firstNum;
-	PhoneNumber secondNum(39, 415, 867666, 'B', 2012);
+/*	PhoneNumber secondNum(39, 415, 867666, 'B', 2012);
 	PhoneNumber thirdNum(1212121);
 	PhoneNumber fourthNum(1234566, 'C');
 	PhoneNumber fifthNum(925, 4392181);
-	PhoneNumber sixthNum(925, 5512346, 'H');
-	/*firstNum.printNumber();
-	firstNum.printPhoneNumberStats();
+*///	PhoneNumber sixthNum(925, 5512346, 'H');
+	firstNum.printNumber();
+/*	firstNum.printPhoneNumberStats();
 	secondNum.printNumber();
 	secondNum.printPhoneNumberStats();
 	thirdNum.printNumber();
